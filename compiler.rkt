80c63a7a78ceb709e3d750cb133b66f04f7d6ecd
Steven A. Watson <66756748+swatson555@users.noreply.github.com>
2021-04-08 21:39:13 -0700

    Solutions For Compiler 1
    Also contains a procedure called compile that will compile, execute, and display the results of the evaluation of it's input expresion.
    
    Signed-off-by: Steven A. Watson <66756748+swatson555@users.noreply.github.com>

--- compiler.rkt
+++ compiler.rkt
@@ -1,6 +1,8 @@
 #lang racket
-(require racket/set racket/stream)
-(require racket/fixnum)
+(require racket/set
+         racket/stream
+         racket/dict
+         racket/fixnum)
 (require "interp-R0.rkt")
 (require "interp-R1.rkt")
 (require "interp.rkt")
@@ -8,74 +10,254 @@
 (provide (all-defined-out))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; R0 examples
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; The following pass is just a silly pass that doesn't change anything important,
-;; but is nevertheless an example of a pass. It flips the arguments of +. -Jeremy
-(define (flipper e)
-  (match e
-    [(? fixnum?) e]
-    [`(read) `(read)]
-    [`(- ,e1) `(- ,(flipper e1))]
-    [`(+ ,e1 ,e2) `(+ ,(flipper e2) ,(flipper e1))]
-    [`(program ,e) `(program ,(flipper e))]
-    ))
-
-
-;; Next we have the partial evaluation pass described in the book.
-(define (pe-neg r)
-  (cond [(fixnum? r) (fx- 0 r)]
-	[else `(- ,r)]))
-
-(define (pe-add r1 r2)
-  (cond [(and (fixnum? r1) (fixnum? r2)) (fx+ r1 r2)]
-	[else `(+ ,r1 ,r2)]))
-
-(define (pe-arith e)
-  (match e
-    [(? fixnum?) e]
-    [`(read) `(read)]
-    [`(- ,e1) (pe-neg (pe-arith e1))]
-    [`(+ ,e1 ,e2) (pe-add (pe-arith e1) (pe-arith e2))]
-    [`(program ,e) `(program ,(pe-arith e))]
-    ))
-
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; HW1 Passes
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
 ;; uniquify : R1 -> R1
+(define (uniquify-exp alist)
+  (lambda (e)
+    (match e
+      [(? symbol?) (dict-ref alist e)]
+      [(? integer?) e]
+      [`(let ([,x ,e]) ,body)
+       (define n (gensym))
+       (define u (uniquify-exp (dict-set alist x n)))
+       `(let ([,n ,(u e)]) ,(u body))]
+      [`(,op ,es ...)
+       `(,op ,@(for/list ([e es]) ((uniquify-exp alist) e)))])))
+
 (define (uniquify e)
-  (error "TODO: code goes here (uniquify)"))
+  (match e
+    [`(program ,info ,exp)
+     `(program ,info ,((uniquify-exp '()) exp))]))
+
 
 ;; remove-complex-opera* : R1 -> R1
+(define (rco-exp e*)
+  (define (make-let-exp x* e* body)
+    (if (null? x*)
+        body
+        `(let ([,(car x*) ,(car e*)])
+           ,(make-let-exp (cdr x*) (cdr e*) body))))
+  (match e*
+    [(? symbol?)  e*]
+    [(? integer?) e*]
+    [`(let ([,x ,e]) ,body)
+     `(let ([,x ,(rco-exp e)]) ,(rco-exp body))]
+    [else
+     (let rco-arg ([e* e*] [t* '()] [te* '()] [re* '()])
+       (if (null? e*)
+           (make-let-exp t* te* (reverse re*))
+           (let ([e (car e*)])
+             (match e
+               [(? symbol?)  (rco-arg (cdr e*) t* te* (cons e re*))]
+               [(? integer?) (rco-arg (cdr e*) t* te* (cons e re*))]
+               [else (define t (gensym))
+                     (rco-arg (cdr e*) (cons t t*) (cons (rco-exp e) te*) (cons t re*))]))))]))
+
 (define (remove-complex-opera* e)
-  (error "TODO: code goes here (remove-complex-opera*)"))
+  (match e
+    [`(program ,info ,exp)
+     `(program ,info ,(rco-exp exp))]))
+
 
 ;; explicate-control : R1 -> C0
+(define (explicate-control-assign x e t)
+  (match e
+    [`(let ([,x0 ,e0]) ,b)
+     (explicate-control-assign x0 e0 (explicate-control-assign x b t))]
+    [else `(seq (assign ,x ,e) ,t)]))
+
+(define (explicate-control-tail e)
+  (match e
+    [`(let ([,x ,e]) ,b)
+     (explicate-control-assign x e (explicate-control-tail b))]
+    [else `(return ,e)]))
+
 (define (explicate-control e)
-  (error "TODO: code goes here (explicate-control)"))
+  (match e
+    [`(program ,info ,exp)
+     `(program ,info ,(dict-set '() 'start (explicate-control-tail exp)))]))
+
 
 ;; uncover-locals : C0 -> C0
+(define (uncover seqs)
+  (if (null? seqs)
+      '()
+      (let loop ([seq (cdar seqs)])
+        (match seq
+          [`(seq (assign ,x ,v) ,tail) (cons x (loop tail))]
+          [`(return ,exp) (uncover (cdr seqs))]))))
+
 (define (uncover-locals e)
-  (error "TODO: code goes here (uncover-locals)"))
+  (match e
+    [`(program ,info ,labels)
+     `(program ,(dict-set info 'locals (uncover labels)) ,labels)]))
+
 
 ;; select-instructions : C0 -> pseudo-x86
+(define (select-instructions-arg arg)
+  (match arg
+    [(? symbol?)  `(var ,arg)]
+    [(? integer?) `(int ,arg)]))
+
+(define (select-instructions-stmt stmt)
+  (match stmt
+    [`(assign ,var (+ ,arg0 ,arg1)) `((movq ,(select-instructions-arg arg0) (var ,var))
+                                      (addq ,(select-instructions-arg arg1) (var ,var)))]
+    [`(assign ,var (- ,arg0))       `((movq ,(select-instructions-arg arg0) (var ,var))
+                                      (negq (var ,var)))]
+    [`(assign ,var (read))          `((callq read_int)
+                                      (movq (reg rax) (var ,var)))]
+    [`(assign ,var ,arg0)           `((movq ,(select-instructions-arg arg0) (var ,var)))]))
+
+(define (select-instructions-tail tail)
+  (match tail
+    [`(return (+ ,arg0 ,arg1)) `((movq ,(select-instructions-arg arg0) (reg rax))
+                                 (addq ,(select-instructions-arg arg1) (reg rax))
+                                 (jmp conclusion))]
+    [`(return (- ,arg0))       `((movq ,(select-instructions-arg arg0) (reg rax))
+                                 (negq (reg rax))
+                                 (jmp conclusion))]
+    [`(return (read))          `((callq read_int)
+                                 (jmp conclusion))]
+    [`(return ,arg0)           `((movq ,(select-instructions-arg arg0) (reg rax))
+                                 (jmp conclusion))]
+    [`(seq ,stmt ,tail)
+     (append (select-instructions-stmt stmt)
+             (select-instructions-tail tail))]))
+
 (define (select-instructions e)
-  (error "TODO: code goes here (select-instructions)"))
+  (match e
+    [`(program ,info ,labels)
+     `(program ,info ,(dict-set labels
+                                'start
+                                (append '(block ()) (select-instructions-tail (dict-ref labels 'start)))))]))
+
 
 ;; assign-homes : pseudo-x86 -> pseudo-x86
+(define (assign-homes-instr homes instrs)
+  (map (lambda (instr)
+         (match instr
+           [`(,op (var ,op0) (var ,op1))
+            `(,op ,(dict-ref homes op0) ,(dict-ref homes op1))]
+           [`(,op (var ,op0) ,op1)
+            `(,op ,(dict-ref homes op0) ,op1)]
+           [`(,op ,op0 (var ,op1))
+            `(,op ,op0 ,(dict-ref homes op1))]
+           [`(,op (var ,op0))
+            `(,op ,(dict-ref homes op0))]
+           [else instr]))
+       instrs))
+
+(define (assign-homes-label info labels)
+  (define (homes vars loc)
+    (if (null? vars)
+        '()
+        (cons (cons (car vars) `(deref rbp ,loc))
+              (homes (cdr vars) (- loc 8)))))
+  (let ([homes-dict (homes (dict-ref info 'locals) -8)])
+    (match (dict-ref labels 'start)
+      [`(block ,b-info ,instr* ...)
+       (dict-set labels 'start (append `(block ,b-info) (assign-homes-instr homes-dict instr*)))])))
+
 (define (assign-homes e)
-  (error "TODO: code goes here (assign-homes)"))
+  (match e
+    [`(program ,info ,labels)
+     `(program ,info ,(assign-homes-label info labels))]))
+
 
 ;; patch-instructions : psuedo-x86 -> x86
+(define (patch-instructions-instr instrs)
+  (if (null? instrs)
+      '()
+      (append (match (car instrs)
+                [`(,op (deref rbp ,loc0) (deref rbp ,loc1))
+                 `((movq (deref rbp ,loc0) (reg rax))
+                   (,op (reg rax) (deref rbp ,loc1)))]
+                [else (list (car instrs))])
+              (patch-instructions-instr (cdr instrs)))))
+
+(define (patch-instructions-label labels)
+  (match (dict-ref labels 'start)
+    [`(block ,b-info ,instr* ...)
+     (dict-set labels 'start (append `(block ,b-info) (patch-instructions-instr instr*)))]))
+
 (define (patch-instructions e)
-  (error "TODO: code goes here (patch-instructions)"))
+  (match e
+    [`(program ,info ,labels)
+     `(program ,info ,(patch-instructions-label labels))]))
+
 
 ;; print-x86 : x86 -> string
+(define (emit-instrs instrs)
+  (if (null? instrs)
+      "\n"
+      (string-append "\t"
+                     (match (car instrs)
+                       [`(jmp ,label) (format "jmp ~a" label)]
+                       [`(callq ,label) (format "callq ~a" label)]
+                       [`(,op (reg ,r)) (format "~a %~a" op r)]
+                       [`(,op (int ,i)) (format "~a $~a" op i)]
+                       [`(,op (deref rbp ,loc)) (format "~a ~a(%rbp)" op loc)]
+                       [`(,op (reg ,r) (int ,i)) (format "~a %~a, $~a" op r i)]
+                       [`(,op (int ,i) (reg ,r)) (format "~a $~a, %~a" op i r)]
+                       [`(,op (reg ,r) (deref rbp ,loc)) (format "~a %~a, ~a(%rbp)" op r loc)]
+                       [`(,op (int ,i) (deref rbp ,loc)) (format "~a $~a, ~a(%rbp)" op i loc)]
+                       [`(,op (deref rbp ,loc) (reg ,r)) (format "~a ~a(%rbp), %~a" op loc r)]
+                       [`(,op (deref rbp ,loc) (int ,i)) (format "~a ~a(%rbp), $~a" op loc i)])
+                     "\n" (emit-instrs (cdr instrs)))))
+
+(define (emit-labels labels)
+  (match (dict-ref labels 'start)
+    [`(block ,b-info ,instr* ...)
+     (string-append "start:\n"
+                    (emit-instrs instr*))]))
+
+(define (emit-prelude size)
+  (format "	.globl main
+main:
+	pushq %rbp
+	movq %rsp, %rbp
+	subq $~a, %rsp
+	jmp start
+conclusion:
+	addq $~a, %rsp
+	popq %rbp
+	retq
+" size size))
+
 (define (print-x86 e)
-  (error "TODO: code goes here (print-x86)"))
+  (match e
+    [`(program ,info ,labels)
+     (define stack-size
+       ;; Align to 16-bytes
+       (let ([size (* 8 (length (dict-ref info 'locals)))])
+         (if (zero? (modulo size 16))
+             size
+             (+ 8 size))))
+     (string-append (emit-labels labels)
+                    (emit-prelude stack-size))]))
+
+(define (compile exp)
+  (define gcc (process "gcc runtime.c -x assembler -"))
+  (define assembler-stdin (list-ref gcc 0))
+  (define assembler-stdout (list-ref gcc 1))
+  (define assembler-id (list-ref gcc 2))
+  (define assembler-stderr (list-ref gcc 3))
+  (define assembler-comm (list-ref gcc 4))
+  (fprintf assembler-stdout
+           (print-x86
+            (patch-instructions
+             (assign-homes
+              (select-instructions
+               (uncover-locals
+                (explicate-control
+                 (remove-complex-opera*
+                  (uniquify `(program () ,exp))))))))))
+  (close-output-port assembler-stdout)
+  (printf (port->string assembler-stdin #:close? #t))
+  (printf (port->string assembler-stderr #:close? #t))
+  (assembler-comm 'wait)
+  (system "./a.out || echo $?"))
